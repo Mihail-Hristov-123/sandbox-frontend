9e3cd72635505edaad0b7419ac86a75de2b8d6be
Mihail-Hristov-123 <mihail.hristov@intellias.com>
2025-10-28 18:41:48 +0200

    chore: set up tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import { NotFound } from './pages/NotFound.tsx'
-import { Routes as availableRoutes } from './Routes.ts'
-import { Home } from './pages/Home.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { NotFound } from "./pages/NotFound.tsx";
+import { Routes as availableRoutes } from "./Routes.ts";
+import { Home } from "./pages/Home.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path={availableRoutes.HOME} element={<Home />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>,
-  </StrictMode>,
-)
+    </BrowserRouter>
+    ,
+  </StrictMode>
+);
