2ab6add0a5c76b7227799ce4eb1e878745912c43
Mihail-Hristov-123 <mihail.hristov@intellias.com>
2025-11-21 14:41:57 +0200

    feat: create heros section basis

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
 export const Home = () => {
     return (
-        <main>
-            <h1>Home page</h1>
+        <main className="min-h-[130vh]">
+            <section className=" bg-cover  bg-fixed h-[70vh] bg-[url('/landing-background.jpg')]">
+                <div className="backdrop-brightness-68 size-full flex flex-col justify-center text-white text-center ">
+                    <h1 className="text-white text-5xl font-extrabold">
+                        Let's go fishing
+                    </h1>
+                    <h2 className="text-2xl">
+                        Find the best spots and tips for your next fishing
+                        adventure
+                    </h2>
+                </div>
+            </section>
         </main>
     );
 };
